0192d9a9545b447ea7f8bb4a55890f50370883d7
callkend <77285430+callkend@users.noreply.github.com>
2021-04-09 00:22:31 -0600

    Add project files.

---
+++ Snake.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Snake
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Snake))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(407, 407)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(438, 28)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Image = CType(resources.GetObject("NewToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.NewToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(175, 26)
+        Me.NewToolStripMenuItem.Text = "&New"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(175, 26)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(142, 26)
+        Me.AboutToolStripMenuItem.Text = "&About..."
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'Snake
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(438, 447)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Snake"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Snake.vb
@@ -0,0 +1,74 @@
+﻿Public Class Snake
+    Enum Directions
+        UP
+        DOWN
+        LEFT
+        RIGHT
+    End Enum
+
+    Dim snakeHead As Point = New Point(8, 8)
+    Dim currentDirection As Int16
+    Dim snakeBody As List(Of Point) = New List(Of Point)
+
+    Sub snakeDraw()
+        Dim g As Graphics = PictureBox1.CreateGraphics
+        Dim lastDirection As Int16
+
+
+        Select Case currentDirection
+            Case Directions.UP
+                snakeHead = New Point(snakeHead.X, snakeHead.Y - 1)
+            Case Directions.DOWN
+                snakeHead = New Point(snakeHead.X, snakeHead.Y + 1)
+            Case Directions.LEFT
+                snakeHead = New Point(snakeHead.X - 1, snakeHead.Y)
+            Case Directions.RIGHT
+                snakeHead = New Point(snakeHead.X + 1, snakeHead.Y)
+        End Select
+        g.FillRectangle(Brushes.Blue, CInt(PictureBox1.Width * (snakeHead.X / 16)), CInt(PictureBox1.Height * (snakeHead.Y / 16)), CInt(PictureBox1.Width / 16), CInt(PictureBox1.Height / 16))
+
+        snakeBody.Insert(0, snakeHead)
+
+        If snakeBody.Count > 4 Then
+            'Erease thing
+            Dim rattle As Point = snakeBody.Last()
+            g.FillRectangle(New SolidBrush(Me.BackColor), CInt(PictureBox1.Width * (rattle.X / 16)), CInt(PictureBox1.Height * (rattle.Y / 16)), CInt(PictureBox1.Width / 16), CInt(PictureBox1.Height / 16))
+
+            snakeBody.Remove(rattle)
+        End If
+    End Sub
+    Sub gameBoard()
+        Dim g As Graphics = PictureBox1.CreateGraphics
+        Dim boardPen As Pen = New Pen(Color.Black)
+
+        For i = 0 To 16
+            g.DrawLine(boardPen, CSng(PictureBox1.Width * (i / 16)), 0, CSng(PictureBox1.Width * (i / 16)), CSng(PictureBox1.Height))
+            g.DrawLine(boardPen, 0, CSng(PictureBox1.Height * (i / 16)), CSng(PictureBox1.Width), CSng(PictureBox1.Height * (i / 16)))
+        Next
+
+        boardPen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Private Sub WASD_Press(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        Dim keyPressed As String = e.KeyChar.ToString
+        Select Case keyPressed
+            Case "w"
+                currentDirection = Directions.UP
+            Case "a"
+                currentDirection = Directions.LEFT
+            Case "s"
+                currentDirection = Directions.DOWN
+            Case "d"
+                currentDirection = Directions.RIGHT
+            Case vbCr
+                gameBoard()
+        End Select
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        snakeDraw()
+
+    End Sub
+End Class
