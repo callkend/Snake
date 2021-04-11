756e9006f1700e4e26a2b65c379901c229601a68
callkend <77285430+callkend@users.noreply.github.com>
2021-04-10 21:16:46 -0600

    Added Header and comments

--- Snake.Designer.vb
+++ Snake.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Snake
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Snake
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Snake))
@@ -33,7 +33,6 @@ Partial Class Snake
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.StartLabel = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -102,29 +101,15 @@ Partial Class Snake
         Me.Timer1.Enabled = True
         Me.Timer1.Interval = 500
         '
-        'StartLabel
-        '
-        Me.StartLabel.AutoSize = True
-        Me.StartLabel.BackColor = System.Drawing.SystemColors.Control
-        Me.StartLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.StartLabel.Location = New System.Drawing.Point(142, 312)
-        Me.StartLabel.Name = "StartLabel"
-        Me.StartLabel.Size = New System.Drawing.Size(144, 20)
-        Me.StartLabel.TabIndex = 4
-        Me.StartLabel.Text = "Press Enter to Start"
-        Me.StartLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.StartLabel.Visible = False
-        '
         'Snake
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(425, 447)
-        Me.Controls.Add(Me.StartLabel)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Name = "Snake"
-        Me.Text = "Form1"
+        Me.Text = "Snake"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
@@ -142,5 +127,4 @@ Partial Class Snake
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Timer1 As Timer
-    Friend WithEvents StartLabel As Label
 End Class
--- Snake.vb
+++ Snake.vb
@@ -1,11 +1,13 @@
-﻿Public Class Snake
+﻿'callkend
+'Snake
+
+Public Class Snake
     Enum Directions
         UP
         DOWN
         LEFT
         RIGHT
     End Enum
-
     Enum GameStates
         PREGAME
         START
@@ -23,6 +25,7 @@
     Const boardSize As Int16 = 16
     Dim Corner As Point = New Point(0, 0)
 
+    'Runs the Start Screen
     Sub PreGame()
         Static flipFlop As Boolean
         Dim g As Graphics = PictureBox1.CreateGraphics
@@ -30,35 +33,46 @@
 
         g.DrawImage(startimage, Corner)
 
+        'Used to flash the Press Enter to Start
         If flipFlop = True Then
             g.DrawString("Press Enter to Start", New Font("Arial", 16), New SolidBrush(Color.Black), New Point(80, 300))
             flipFlop = False
             Return
         End If
         flipFlop = True
-        'StartLabel.Visible = Not StartLabel.Visible
+
         g.Dispose()
     End Sub
 
+    'Handles setting the game up
     Sub StartGame()
         Randomize()
         Dim g As Graphics = PictureBox1.CreateGraphics
 
+        'Draws Game board
         g.Clear(PictureBox1.BackColor)
-        StartLabel.Visible = False
         GameBoard()
-        '
+
         gameStop = False
+
+        'Resets Snake points
         snakeHead = New Point(8, 8)
         snakeLength = 4
+
+        'Draws Cherry
         cherry = New Point(Rnd(1) * boardSize, Rnd(1) * boardSize)
         g.FillRectangle(New SolidBrush(Color.OrangeRed), CInt(PictureBox1.Width * (cherry.X / boardSize)), CInt(PictureBox1.Height * (cherry.Y / boardSize)), CInt(PictureBox1.Width / boardSize), CInt(PictureBox1.Height / boardSize))
+
+        'Tells game to run
         gameState = GameStates.RUN
         g.Dispose()
     End Sub
 
+    'Handles drawing and erasing the snake body
     Sub snakeDraw()
         Dim g As Graphics = PictureBox1.CreateGraphics
+
+        'Handles changing the snake heads position
         If gameStop = False Then
             Select Case currentDirection
                 Case Directions.UP
@@ -70,10 +84,14 @@
                 Case Directions.RIGHT
                     snakeHead = New Point(snakeHead.X + 1, snakeHead.Y)
             End Select
+
+            'Draws Snake Head
             g.FillRectangle(Brushes.Blue, CInt(PictureBox1.Width * (snakeHead.X / boardSize) + 1), CInt(PictureBox1.Height * (snakeHead.Y / boardSize) + 1), CInt(PictureBox1.Width / boardSize) - 1, CInt(PictureBox1.Height / boardSize) - 1)
 
+            'Puts heads position into a list.
             snakeBody.Insert(0, snakeHead)
 
+            'Erases the snake body based on the last position in the list
             If snakeBody.Count > snakeLength Then
                 'Erase thing
                 Dim rattle As Point = snakeBody.Last()
@@ -82,10 +100,12 @@
                 snakeBody.Remove(rattle)
             End If
         End If
+
         Collision()
         g.Dispose()
     End Sub
 
+    'Handles any time the snake head encounters a wall, body, or cherry
     Sub Collision()
         Dim g As Graphics = PictureBox1.CreateGraphics
         'Handles Cherry Collision
@@ -116,6 +136,7 @@
         Dim g As Graphics = PictureBox1.CreateGraphics
         Dim boardPen As Pen = New Pen(Color.Black)
 
+        'Draws game board 
         For i = 0 To boardSize
             g.DrawLine(boardPen, CSng(PictureBox1.Width * (i / boardSize)), 0, CSng(PictureBox1.Width * (i / boardSize)), CSng(PictureBox1.Height))
             g.DrawLine(boardPen, 0, CSng(PictureBox1.Height * (i / boardSize)), CSng(PictureBox1.Width), CSng(PictureBox1.Height * (i / boardSize)))
@@ -125,6 +146,7 @@
         g.Dispose()
     End Sub
 
+    'Handles the game over screen
     Sub GameOver()
         Dim g As Graphics = PictureBox1.CreateGraphics
         Dim overImage As Image = Image.FromFile("C:\Users\callk\source\repos\Snake\SnakeGameoverScreen.png")
@@ -136,6 +158,7 @@
         overImage.Dispose()
     End Sub
 
+    'Handles user inputs
     Private Sub WASD_Press(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
         Dim keyPressed As String = e.KeyChar.ToString
         Select Case keyPressed
@@ -159,6 +182,7 @@
                     Return
                 End If
                 currentDirection = Directions.RIGHT
+               'If user hits enter the game will move to the next state if in Pregame or Stopgame
             Case vbCr
                 If gameState = GameStates.PREGAME Then
                     gameState = GameStates.START
@@ -169,6 +193,7 @@
         End Select
     End Sub
 
+    'Game Tick
     Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Select Case gameState
             Case GameStates.PREGAME
